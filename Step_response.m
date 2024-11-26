0416b27929ae33c241a76a43e667076d790455d2
SoumyadeepMukherjee14 <121843024+SoumyadeepMukherjee14@users.noreply.github.com>
2024-11-26 20:53:50 +0530

    Update Step_response.m

--- Step_response.m
+++ Step_response.m
@@ -1,9 +1,9 @@
-Ra = 0.45;                 %Per phase stator resistance
-La = 0.035;              %Per phase stator inductance
-B = 2e-4;              %Damping coefficient
-J = 0.022;             %Moment of Inertia
-Ke = 0.5;                %Torque c0nstant
-Kt = 0.5;                %Back EMF constant
+Ra = 0.45;     %Per phase stator resistance
+La = 0.035;    %Per phase stator inductance
+B = 2e-4;      %Damping coefficient
+J = 0.022;     %Moment of Inertia
+Ke = 0.5;      %Torque c0nstant
+Kt = 0.5;      %Back EMF constant
 
 Kp =  4.19652540503482;
 Ki =  35.8889727302617;
@@ -18,8 +18,8 @@ s=tf('s');
 
 G = tf([0 0 Kt],[a b c])
 
-[w,zeta] = damp(G)  % zeta calculation for transfer function
-C = Kp + Ki/s + ((Kd*s*N)/(s+N)); %PID Controller 
+[w,zeta] = damp(G)                  % zeta calculation for transfer function
+C = Kp + Ki/s + ((Kd*s*N)/(s+N));   %PID Controller 
 
 H = feedback(C*G,1);
 step(H) 
